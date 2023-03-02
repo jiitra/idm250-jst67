d2d9d1947c8bb2291d9df834c477c6a75cadd7c8
jiitra <74521649+jiitra@users.noreply.github.com>
2023-03-02 15:08:41 -0500

    featured post section

--- wp-content/themes/gemster/page.php
+++ wp-content/themes/gemster/page.php
@@ -2,6 +2,22 @@
 
 <?php get_template_part('components/heros/hero-page'); ?>
 
+<!-- static until i figure it out -->
+<div class="featured-post">
+
+   <!-- placeholder -->
+   <?php $image = get_field('hero_image'); ?>
+   <div class="featured-post-img-container">
+   <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+   </div>
+   
+   <h3> <em>Featured Post</em> </h3>
+   <p>I want to share my daily routine with you all.</p>
+   <button>Read More</button>
+
+</div>
+
+
 <?php get_template_part('components/content'); ?>
 
 <?php
