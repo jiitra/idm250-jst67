2b17506750e09913d6951b5010a8ee73c8b7452e
jiitra <74521649+jiitra@users.noreply.github.com>
2023-03-09 19:48:57 -0500

    sidebar

--- wp-content/themes/gemster/templates/sidebar.php
+++ wp-content/themes/gemster/templates/sidebar.php
@@ -1,5 +1,17 @@
 <?php
-
 /* Template Name: Page with Sidebar */
+get_header();
 
-echo 'this page is template';
+get_template_part('components/heros/hero-page'); ?>
+
+
+
+<div class="" data-component="sidebar-template">
+  <div>
+    <?php get_template_part('components/content'); ?>
+  </div>
+  <div class="sidebar">
+    <?php get_template_part('components/sidebar'); ?>
+  </div>
+</div>
+<?php get_footer(); ?>
