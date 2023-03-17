714cc49857f7cc44f130f4a805d12ae1387f6a77
jiitra <74521649+jiitra@users.noreply.github.com>
2023-03-16 22:12:21 -0400

    actually accomplished nothing

--- wp-content/themes/gemster/header.php
+++ wp-content/themes/gemster/header.php
@@ -14,13 +14,16 @@
 
   <?php wp_body_open(); ?>
 
-  <header>
+<header>
+
+  <div class="nav-menu">
     <div class="header-content">
       <a href="<?php echo site_url(); ?>">
         <img src="<?php echo get_template_directory_uri(); ?>/dist/img/gemster-brand.svg" alt="Logo">
-        <?php wp_nav_menu(['theme_location' => 'primary-menu']); ?>
       </a>
 
+      <?php wp_nav_menu(['theme_location' => 'primary-menu']); ?>
+    
       <div>
         <a href="https://twitter.com/home" target="_blank">
           <img id="twitter" src="<?php echo get_template_directory_uri(); ?>/dist/img/twitter-brand.svg" alt="Twitter">
@@ -29,6 +32,12 @@
           <img src="<?php echo get_template_directory_uri(); ?>/dist/img/insta-brand.svg" alt="Instagram">
         </a>
       </div>
-    </div>
 
+    <div class="ham-menu">
+      <svg viewBox="0 0 100 80" width="40" height="40">
+        <rect width="100" height="20"></rect>
+        <rect y="30" width="100" height="20"></rect>
+        <rect y="60" width="100" height="20"></rect>
+      </svg>
+    </div>
   </header>
